bf5fdc6210f4a6e0e8dec2d5beaf8497c45409da
JobKhy <pina.martinez.gael.job@gmail.com>
2025-04-02 22:26:50 -0600

    bump: update version to 1.0.3 for platform-specific packages and 1.0.4 for main package

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,8 @@
 #![deny(clippy::all)]
-use image::{open, ImageFormat};
+use image::ImageFormat;
 use napi::bindgen_prelude::*;
 use napi_derive::napi;
 use std::io::Cursor;
-use std::path::Path;
-
-#[macro_use]
-extern crate napi_derive;
-
-#[napi]
-pub fn convert_image(input_path: String, output_path: String, format: String) -> Result<()> {
-  let img =
-    open(&input_path).map_err(|_| Error::new(Status::GenericFailure, "Error loading image"))?;
-
-  let output_format = match format.as_str() {
-    "png" => ImageFormat::Png,
-    "jpg" | "jpeg" => ImageFormat::Jpeg,
-    "bmp" => ImageFormat::Bmp,
-    "gif" => ImageFormat::Gif,
-    "tiff" => ImageFormat::Tiff,
-    "webp" => ImageFormat::WebP,
-    _ => return Err(Error::new(Status::InvalidArg, "Unsupported format")),
-  };
-
-  img
-    .save_with_format(Path::new(&output_path), output_format)
-    .map_err(|_| Error::new(Status::GenericFailure, "Error saving image"))?;
-
-  Ok(())
-}
 
 #[napi]
 pub fn convert_image_file(input_data: Buffer, format: String) -> Result<Buffer> {
